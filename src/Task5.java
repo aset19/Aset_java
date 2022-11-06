80f48fa96e9d47a0710b21fd86225d9f5d8dc4a9
Aset <vip.askenov@mail.ru>
2022-11-06 23:32:49 +0300

    дз

---
+++ src/Task5.java
@@ -0,0 +1,11 @@
+public class Task5 {
+    public static void main(String[] args){
+        Cat cat = new Cat();
+        cat.setName("Китти");
+        cat.setGrowth(50);
+        cat.setWeight(18);
+        cat.setCountry(1);
+        System.out.println(cat.toString());
+    }
+    }
+
