69b8d0350d5d8947e96e560a146d32c52e78e8ef
Aset <vip.askenov@mail.ru>
2022-11-06 23:43:09 +0300

    дз

--- src/Task5.java
+++ src/Task5.java
@@ -9,3 +9,65 @@ public class Task5 {
     }
     }
 
+    class Cat{
+    private String name;
+    private int growth;
+    private int weight;
+    private int country;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrowth() {
+        return growth;
+    }
+
+    public void setGrowth(int growth) {
+        this.growth = growth;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getCountry() {
+        return country;
+    }
+    @Override
+    public String toString() {
+        return  "name='" + name + '\n' +
+                "growth=" + growth + '\n' +
+                "weight=" + weight + '\n' +
+                "country=" + country + '\n';
+    }
+
+    public void setCountry(int country) {
+        this.country = country;
+        switch (country){
+            case 1:
+                System.out.println( "Country" + " Россия");
+                break;
+            case 2:
+                System.out.println("Country" + " Канада");
+                break;
+            case 3:
+                System.out.println("Country"+ " Египет");
+                break;
+            default:
+                System.out.println("ХЗ откуда");
+
+        }
+    }
+}
+
+
+
